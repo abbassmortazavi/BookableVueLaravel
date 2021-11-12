b96f83d3619626bbdc294f5fe2cc2957dec3954d
Abbass <abbassmortazavi@gmail.com>
2021-11-12 17:41:28 +0330

    create review table model

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Review;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReviewFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Review::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id'=> Str::uuid(),
+            'content'=> $this->faker->sentence(5,true),
+            'rating'=> rand(1,5)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(BookableTableSeeder::class);
         $this->call(BookingsSeeder::class);
+        $this->call(ReviewsTableSeeder::class);
     }
 }
